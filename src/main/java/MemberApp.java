ba627528fb0e671d09c3d04148a9106bc5c724d5
SHIN__0.0 <ajtwoddltom@gmail.com>
2025-01-04 20:47:54 +0900

    Java-code

---
+++ src/main/java/MemberApp.java
@@ -0,0 +1,9 @@
+import member.MemberService;
+import member.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+    }
+}
