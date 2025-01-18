af4540d7acfc2b3d3e93227d6513b2c65822f272
SHIN__0.0 <ajtwoddltom@gmail.com>
2025-01-18 23:08:17 +0900

    unusing connection

---
+++ src/test/java/com/example/springbasic/lifecycle/NetworkClient.java
@@ -0,0 +1,46 @@
+package com.example.springbasic.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
+
+    private String url;
+
+    public NetworkClient(){
+        System.out.println("생성자 호출, url = " + url);
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    //서비스 시작시 호출
+    public void connect() {
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String message){
+        System.out.println("call: " + url + " message = " + message);
+    }
+
+    //서비스 종료시 호출
+    public void disconnect() {
+        System.out.println("close " + url);
+    }
+
+    //스프링 컨테이너 올라올때 생성 후 의존관계 주입 끝나면 호출
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    //빈 종료 될때
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
+    }
+}
