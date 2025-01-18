03d1ea78678224dcc20e5da8de8d1bdc40a78f1e
SHIN__0.0 <ajtwoddltom@gmail.com>
2025-01-18 19:53:15 +0900

    making annotation

--- src/main/java/com/example/springbasic/order/OrderServiceImpl.java
+++ src/main/java/com/example/springbasic/order/OrderServiceImpl.java
@@ -1,15 +1,16 @@
 package com.example.springbasic.order;
 
+import com.example.springbasic.annotation.MainDiscountPolicy;
 import com.example.springbasic.discount.DiscountPolicy;
 import com.example.springbasic.member.Member;
 import com.example.springbasic.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
@@ -18,6 +19,12 @@ public class OrderServiceImpl implements OrderService {
     private final DiscountPolicy discountPolicy;
     //이렇게하면 인터페이스만 의존, 구현체에 의존하지 않음
     //여기서 할당하는 대신에 OrderServiceImpl에 구현 객체를 대신 생성하고 주입시켜주어야한다.
+
+
+    public OrderServiceImpl(MemberRepository memberRepository,@MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
